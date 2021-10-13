38211905bcaac0aedc4062adbb2d67f8bf723f8d
Awad2002 <awadamr33@gmail.com>
2021-10-13 10:35:59 +0300

    ahmad

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 $('#owl-one').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:true,
-    navText: ["<img src='img/right-arrow.png'>","<img src='img/left-arrow.png'>"],
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: ["<img src='img/right-arrow.png'>", "<img src='img/left-arrow.png'>"],
     rtl: true,
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:6
+        1000: {
+            items: 6
         }
     }
 });
 
 $('#owl-two').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:true,
-    navText: ["<img src='img/right-arrow.png'>","<img src='img/left-arrow.png'>"],
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: ["<img src='img/right-arrow.png'>", "<img src='img/left-arrow.png'>"],
     rtl: true,
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:4
+        1000: {
+            items: 4
         }
     }
 });
@@ -48,9 +48,9 @@ $(".navbar .search-btn").click(function() {
     $('form.search').toggle();
 });
 
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar.sticky-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $(".navbar.sticky-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height(40));
     });
 });
